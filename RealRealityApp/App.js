7191acd184d7fcbe4cfef734288a0632b6408886
tanguycoenen <tanguy.coenen@gmail.com>
2018-09-02 20:06:40 +0200

    push notifications using expo

--- RealRealityApp/App.js
+++ RealRealityApp/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
-import {StyleSheet, Text, View  } from 'react-native';
+import {
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  KeyboardAvoidingView,
+  View,
+} from 'react-native';
+import { Permissions, Notifications } from 'expo';
 
 export default class RealReality extends Component {
+
   constructor(props){
     super(props);
     this.state = {
@@ -9,8 +18,53 @@ export default class RealReality extends Component {
       latitude: null,
       longitude: null,
       error: null,
+      token: null,
+      notification: null,
+      title: 'Hello World',
+      body: 'Say something!',
     };
   }
+
+  async registerForPushNotifications() {
+    const { status } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+
+    if (status !== 'granted') {
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      if (status !== 'granted') {
+        return;
+      }
+    }
+
+    const token = await Notifications.getExpoPushTokenAsync();
+
+    this.subscription = Notifications.addListener(this.handleNotification);
+
+    this.setState({
+      token,
+    });
+  }
+
+  sendPushNotification(token = this.state.token, title = this.state.title, body = this.state.body) {
+    return fetch('https://exp.host/--/api/v2/push/send', {
+      body: JSON.stringify({
+        to: token,
+        title: title,
+        body: body,
+        data: { message: `${title} - ${body}` },
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+    });
+  }
+
+  handleNotification = notification => {
+    this.setState({
+      notification,
+    });
+  };
+
   componentDidMount(){
     //return fetch('https://35.158.121.141/51.1987722/4.4234877')
     /*return fetch('https://facebook.github.io/react-native/movies.json')
@@ -57,7 +111,49 @@ export default class RealReality extends Component {
         <Text>Latitude: {this.state.latitude}</Text>
         <Text>Longitude: {this.state.longitude}</Text>
        {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
+       <KeyboardAvoidingView style={styles.container} behavior="position">
+               <Text style={styles.title}>Expo Sample Notifications App</Text>
+               <Text style={styles.text}>Title</Text>
+               <TextInput
+                 style={styles.input}
+                 onChangeText={title => this.setState({ title })}
+                 maxLength={100}
+                 value={this.state.title}
+               />
+               <Text style={styles.text}>Message</Text>
+               <TextInput
+                 style={styles.input}
+                 onChangeText={body => this.setState({ body })}
+                 maxLength={100}
+                 value={this.state.body}
+               />
+               <TouchableOpacity
+                 onPress={() => this.registerForPushNotifications()}
+                 style={styles.touchable}>
+                 <Text>Register me for notifications!</Text>
+               </TouchableOpacity>
+               <TouchableOpacity onPress={() => this.sendPushNotification()} style={styles.touchable}>
+                 <Text>Send me a notification!</Text>
+               </TouchableOpacity>
+               {this.state.token ? (
+                 <View>
+                   <Text style={styles.text}>Token</Text>
+                   <TextInput
+                     style={styles.input}
+                     onChangeText={token => this.setState({ token })}
+                     value={this.state.token}
+                   />
+                 </View>
+               ) : null}
+               {this.state.notification ? (
+                 <View>
+                   <Text style={styles.text}>Last Notification:</Text>
+                   <Text style={styles.text}>{JSON.stringify(this.state.notification.data.message)}</Text>
+                 </View>
+               ) : null}
+             </KeyboardAvoidingView>
       </View>
+
     );
   }
 }
@@ -67,5 +163,31 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: 18,
+    padding: 8,
+  },
+  text: {
+    paddingBottom: 2,
+    padding: 8,
+  },
+  container: {
+    flex: 1,
+    paddingTop: 40,
+  },
+  touchable: {
+    borderWidth: 1,
+    borderRadius: 4,
+    margin: 8,
+    padding: 8,
+    width: '95%',
+  },
+  input: {
+    height: 40,
+    borderWidth: 1,
+    margin: 8,
+    padding: 8,
+    width: '95%',
   },
 });
